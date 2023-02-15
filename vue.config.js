b99fb559a5d3f39811837b510087b0249fc37a03
Duncan Kiragu <32220225+Duncan-Kiragu@users.noreply.github.com>
2023-02-15 15:19:59 +0300

    replace repo name

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  publicPath: process.env.NODE_ENV === 'production' ? '/tripday-vue/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/frontend-demo/' : '/',
 }
